7b9ca7c02c0010c154c7046a3624741183b3abc3
filipegraciosainsburys <filipe.gracio@sainsburys.co.uk>
2018-09-28 18:48:35 +0100

    some evening changes

--- tests/690/test_sarsa.py
+++ tests/690/test_sarsa.py
@@ -41,44 +41,49 @@ def test_episode_simulator_with_print():
 def test_sarsa_creates_better_policy():
     n_sims = 200
     sarsa_episodes = 10000
+    grid_shape = (10, 7)
+    final_position = (7, 3)
+    initial_position = (0, 3)
 
 
     ## bad
-    environment_bad, agent_bad = create_environment_agent_and_states(grid_shape=(5, 5),
-                                                             initial_position=(0, 2),
-                                                             final_position=(2, 4),
-                                                             epsilon=0.2)
+    environment_bad, agent_bad = create_environment_agent_and_states(grid_shape=grid_shape,
+                                                                     initial_position=initial_position,
+                                                                     final_position=final_position,
+                                                                     epsilon=0.1)
     sarsa_simulator_bad = Sarsa(alpha=0.5, gama=0.9)
     environment_bad, agent_bad = sarsa_simulator_bad.run_sarsa(environment=environment_bad,
-                                                   agent=agent_bad,
-                                                   initial_state=(0, 2),
-                                                   n_episodes=10)
+                                                               agent=agent_bad,
+                                                               initial_state=initial_position,
+                                                               n_episodes=10)
     bad_list = []
     for i in range(n_sims):
         episode_simulator = EpisodeSimulator(agent=agent_bad, environment=environment_bad)
-        state_action_tuple_list_bad, reward_list_bad = episode_simulator.run_episode(start_state=(0, 2))
+        state_action_tuple_list_bad, reward_list_bad = episode_simulator.run_episode(start_state=initial_position)
         bad_list.append(sum(reward_list_bad))
 
 
 
     ## good
-    environment_good, agent_good = create_environment_agent_and_states(grid_shape=(5, 5),
-                                                                     initial_position=(0, 2),
-                                                                     final_position=(2, 4),
-                                                                     epsilon=0.2)
+    environment_good, agent_good = create_environment_agent_and_states(grid_shape=grid_shape,
+                                                                       initial_position=initial_position,
+                                                                       final_position=final_position,
+                                                                       epsilon=0.1)
     sarsa_simulator_good = Sarsa(alpha=0.5, gama=0.9)
     environment_good, agent_good = sarsa_simulator_good.run_sarsa(environment=environment_good,
-                                                               agent=agent_good,
-                                                               initial_state=(0, 2),
-                                                               n_episodes=sarsa_episodes)
+                                                                  agent=agent_good,
+                                                                  initial_state=initial_position,
+                                                                  n_episodes=sarsa_episodes)
     good_list = []
     for i in range(n_sims):
         episode_simulator = EpisodeSimulator(agent=agent_good, environment=environment_good)
-        state_action_tuple_list_good, reward_list_good = episode_simulator.run_episode(start_state=(0, 2))
+        state_action_tuple_list_good, reward_list_good = episode_simulator.run_episode(start_state=initial_position)
         good_list.append(sum(reward_list_good))
     
     print('bad mean', np.mean(bad_list))
     print('bad sd', np.std(bad_list))
     print('good mean', np.mean(good_list))
     print('good sd', np.std(good_list))
+    print(state_action_tuple_list_good)
+    print(agent_good.q_dict)
 
